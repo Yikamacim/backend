0881c20c88a020a05e7b2204da236e1b1976ff83
EmrecanKaracayir <developer@karacayir.com>
2025-03-15 17:51:56 +0300

    Updated: name surname.

--- source/core/login/schemas/LoginResponse.ts
+++ source/core/login/schemas/LoginResponse.ts
@@ -6,12 +6,21 @@ export class LoginResponse implements IResponse {
   public constructor(
     public readonly accountId: number,
     public readonly phone: string,
+    public readonly name: string,
+    public readonly surname: string,
     public readonly accountType: AccountType,
     public readonly isVerified: boolean,
   ) {}
 
   public static fromModel(model: AccountModel): LoginResponse {
-    return new LoginResponse(model.accountId, model.phone, model.accountType, model.isVerified);
+    return new LoginResponse(
+      model.accountId,
+      model.phone,
+      model.name,
+      model.surname,
+      model.accountType,
+      model.isVerified,
+    );
   }
 
   public static fromModels(models: AccountModel[]): LoginResponse[] {
