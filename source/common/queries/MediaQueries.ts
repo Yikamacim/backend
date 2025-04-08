5bc9a77e81a4a011447ab39d48c904f1eb47b014
EmrecanKaracayir <developer@karacayir.com>
2025-04-08 05:20:26 +0300

    Fixed: binding in purge task

--- source/common/queries/MediaQueries.ts
+++ source/common/queries/MediaQueries.ts
@@ -1,7 +1,7 @@
 export enum MediaQueries {
   GET_MEDIAS = `SELECT * FROM "Media"`,
   GET_MEDIAS_$ACID_$ISUS = `SELECT * FROM "Media" WHERE "accountId" = $1 AND "isUsed" = $2`,
-  DELETE_MEDIAS_$ISUS_$EXPR = `DELETE FROM "Media" WHERE "isUsed" = $1 AND "createdAt" < NOW() - INTERVAL '$2 hours'`,
+  DELETE_MEDIAS_$ISUS_$EXPR = `DELETE FROM "Media" WHERE "isUsed" = $1 AND "createdAt" < NOW() - INTERVAL '1 hour' * $2`,
 
   INSERT_MEDIA_RT_$ACID_$MTYP_$EXTN = `INSERT INTO "Media" ("accountId", "mediaType", "extension") VALUES ($1, $2::"MediaType", $3) RETURNING *`,
   UPDATE_MEDIA_$MDID_$ISUS = `UPDATE "Media" SET "isUsed" = $2 WHERE "mediaId" = $1`,
