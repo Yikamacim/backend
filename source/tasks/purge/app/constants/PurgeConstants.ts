b4ad1784cd207c1edfc265cfd7f5c6ed0348ad1f
EmrecanKaracayir <developer@karacayir.com>
2025-04-08 05:25:48 +0300

    Changed: unused media expiration time

--- source/tasks/purge/app/constants/PurgeConstants.ts
+++ source/tasks/purge/app/constants/PurgeConstants.ts
@@ -2,5 +2,5 @@ import type { IConstants } from "../../../../app/interfaces/IConstants";
 
 export class PurgeConstants implements IConstants {
   public static readonly CRON_SCHEDULE = "0 6 * * *"; // Every day at 6 AM
-  public static readonly UNUSED_MEDIA_EXPIRATION_TIME = 12; // 12 hours
+  public static readonly UNUSED_MEDIA_EXPIRATION_TIME = 6; // 6 hours
 }
