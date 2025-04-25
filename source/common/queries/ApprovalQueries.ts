0f12b86cdb64b1e367dd0ac95f53ccd88919d712
EmrecanKaracayir <developer@karacayir.com>
2025-04-25 17:44:06 +0300

    Fixed: approval update

--- source/common/queries/ApprovalQueries.ts
+++ source/common/queries/ApprovalQueries.ts
@@ -3,6 +3,6 @@ export enum ApprovalQueries {
 
   GET_APPROVAL_$BSID = `SELECT * FROM "Approval" WHERE "businessId" = $1`,
   INSERT_APPROVAL_$BSID_$MSG = `INSERT INTO "Approval" ("businessId", "message") VALUES ($1, $2) RETURNING *`,
-  UPDATE_APPROVAL_$BSID_$STATE_$REASON = `UPDATE "Approval" SET "approvalState" = $1::"ApprovalState", "reason" = $2 WHERE "businessId" = $3 RETURNING *`,
+  UPDATE_APPROVAL_$BSID_$STATE_$REASON = `UPDATE "Approval" SET "approvalState" = $2::"ApprovalState", "reason" = $3 WHERE "businessId" = $1 RETURNING *`,
   DELETE_APPROVAL_$BSID = `DELETE FROM "Approval" WHERE "businessId" = $1`,
 }
