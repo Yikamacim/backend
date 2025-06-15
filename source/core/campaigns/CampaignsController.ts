19403f919033ed5c6da1eef510c719539947801e
EmrecanKaracayir <developer@karacayir.com>
2025-06-15 20:49:29 +0300

    Fixup Campaigns

--- source/core/campaigns/CampaignsController.ts
+++ source/core/campaigns/CampaignsController.ts
@@ -1,47 +1,47 @@
-import type { ControllerResponse } from "../../@types/responses";
-import type { ExpressNextFunction, ExpressRequest } from "../../@types/wrappers";
-import type { IController } from "../../app/interfaces/IController";
-import { HttpStatus, HttpStatusCode } from "../../app/schemas/HttpStatus";
-import { ResponseUtil } from "../../app/utils/ResponseUtil";
-import { CampaignsManager } from "./CampaignsManager";
-import { CampaignsParams } from "./schemas/CampaignsQueries";
-import type { CampaignsResponse } from "./schemas/CampaignsResponse";
+// import type { ControllerResponse } from "../../@types/responses";
+// import type { ExpressNextFunction, ExpressRequest } from "../../@types/wrappers";
+// import type { IController } from "../../app/interfaces/IController";
+// import { HttpStatus, HttpStatusCode } from "../../app/schemas/HttpStatus";
+// import { ResponseUtil } from "../../app/utils/ResponseUtil";
+// import { CampaignsManager } from "./CampaignsManager";
+// import { CampaignsParams } from "./schemas/CampaignsQueries";
+// import type { CampaignsResponse } from "./schemas/CampaignsResponse";
 
-export class CampaignsController implements IController {
-  public constructor(private readonly manager = new CampaignsManager()) {}
+// export class CampaignsController implements IController {
+//   public constructor(private readonly manager = new CampaignsManager()) {}
 
-  public async getCampaigns(
-    req: ExpressRequest,
-    res: ControllerResponse<CampaignsResponse[] | null, null>,
-    next: ExpressNextFunction,
-  ): Promise<typeof res | void> {
-    try {
-      // >----------< VALIDATION >----------<
-      const params = CampaignsParams.parse(req);
-      if (params.clientErrors.length > 0 || params.data === null) {
-        return ResponseUtil.controllerResponse(
-          res,
-          new HttpStatus(HttpStatusCode.BAD_REQUEST),
-          null,
-          params.clientErrors,
-          [],
-          null,
-        );
-      }
-      // >-----------< LOGIC >-----------<
-      const out = await this.manager.getCampaigns(params.data);
-      // >-----------< RESPONSE >-----------<
-      return ResponseUtil.controllerResponse(
-        res,
-        out.httpStatus,
-        out.serverError,
-        out.clientErrors,
-        out.data,
-        null,
-        false,
-      );
-    } catch (error) {
-      return next(error);
-    }
-  }
-}
+//   public async getCampaigns(
+//     req: ExpressRequest,
+//     res: ControllerResponse<CampaignsResponse[] | null, null>,
+//     next: ExpressNextFunction,
+//   ): Promise<typeof res | void> {
+//     try {
+//       // >----------< VALIDATION >----------<
+//       const params = CampaignsParams.parse(req);
+//       if (params.clientErrors.length > 0 || params.data === null) {
+//         return ResponseUtil.controllerResponse(
+//           res,
+//           new HttpStatus(HttpStatusCode.BAD_REQUEST),
+//           null,
+//           params.clientErrors,
+//           [],
+//           null,
+//         );
+//       }
+//       // >-----------< LOGIC >-----------<
+//       const out = await this.manager.getCampaigns(params.data);
+//       // >-----------< RESPONSE >-----------<
+//       return ResponseUtil.controllerResponse(
+//         res,
+//         out.httpStatus,
+//         out.serverError,
+//         out.clientErrors,
+//         out.data,
+//         null,
+//         false,
+//       );
+//     } catch (error) {
+//       return next(error);
+//     }
+//   }
+// }
--- source/core/campaigns/CampaignsProvider.ts
+++ source/core/campaigns/CampaignsProvider.ts
@@ -1,43 +1,43 @@
-import type { ProviderResponse } from "../../@types/responses";
-import { DbConstants } from "../../app/constants/DbConstants";
-import type { IProvider } from "../../app/interfaces/IProvider";
-import { ResponseUtil } from "../../app/utils/ResponseUtil";
-import { BusinessServiceViewModel } from "../../common/models/BusinessServiceViewModel";
-import { BusinessMediaProvider } from "../../common/providers/BusinessMediaProvider";
-import { BusinessAreaQueries } from "../../common/queries/BusinessAreaQueries";
-import { BusinessServiceViewQueries } from "../../common/queries/BusinessServiceViewQueries";
+// import type { ProviderResponse } from "../../@types/responses";
+// import { DbConstants } from "../../app/constants/DbConstants";
+// import type { IProvider } from "../../app/interfaces/IProvider";
+// import { ResponseUtil } from "../../app/utils/ResponseUtil";
+// import { BusinessServiceViewModel } from "../../common/models/BusinessServiceViewModel";
+// import { BusinessMediaProvider } from "../../common/providers/BusinessMediaProvider";
+// import { BusinessAreaQueries } from "../../common/queries/BusinessAreaQueries";
+// import { BusinessServiceViewQueries } from "../../common/queries/BusinessServiceViewQueries";
 
-export class CampaignsProvider implements IProvider {
-  public constructor(private readonly businessMediaProvider = new BusinessMediaProvider()) {
-    this.getBusinessMedia = this.businessMediaProvider.getBusinessMedia.bind(
-      this.businessMediaProvider,
-    );
-  }
+// export class CampaignsProvider implements IProvider {
+//   public constructor(private readonly businessMediaProvider = new BusinessMediaProvider()) {
+//     this.getBusinessMedia = this.businessMediaProvider.getBusinessMedia.bind(
+//       this.businessMediaProvider,
+//     );
+//   }
 
-  public readonly getBusinessMedia: typeof this.businessMediaProvider.getBusinessMedia;
+//   public readonly getBusinessMedia: typeof this.businessMediaProvider.getBusinessMedia;
 
-  public async campaignsBusinesses(
-    neighborhoodId: number,
-    query: string,
-  ): Promise<ProviderResponse<BusinessServiceViewModel[]>> {
-    await DbConstants.POOL.query(DbConstants.BEGIN);
-    try {
-      const results = await DbConstants.POOL.query(
-        BusinessServiceViewQueries.CAMPAIGNS_BUSINESSES_$BSIDS_$QUERY,
-        [
-          (
-            await DbConstants.POOL.query(BusinessAreaQueries.GET_BUSINESS_IDS_BY_NEIGHBORHOOD_ID, [
-              neighborhoodId,
-            ])
-          ).rows.map((row: { businessId: number }) => row.businessId),
-          query,
-        ],
-      );
-      const records: unknown[] = results.rows;
-      return await ResponseUtil.providerResponse(BusinessServiceViewModel.fromRecords(records));
-    } catch (error) {
-      await DbConstants.POOL.query(DbConstants.ROLLBACK);
-      throw error;
-    }
-  }
-}
+//   public async campaignsBusinesses(
+//     neighborhoodId: number,
+//     query: string,
+//   ): Promise<ProviderResponse<BusinessServiceViewModel[]>> {
+//     await DbConstants.POOL.query(DbConstants.BEGIN);
+//     try {
+//       const results = await DbConstants.POOL.query(
+//         BusinessServiceViewQueries.CAMPAIGNS_BUSINESSES_$BSIDS_$QUERY,
+//         [
+//           (
+//             await DbConstants.POOL.query(BusinessAreaQueries.GET_BUSINESS_IDS_BY_NEIGHBORHOOD_ID, [
+//               neighborhoodId,
+//             ])
+//           ).rows.map((row: { businessId: number }) => row.businessId),
+//           query,
+//         ],
+//       );
+//       const records: unknown[] = results.rows;
+//       return await ResponseUtil.providerResponse(BusinessServiceViewModel.fromRecords(records));
+//     } catch (error) {
+//       await DbConstants.POOL.query(DbConstants.ROLLBACK);
+//       throw error;
+//     }
+//   }
+// }
